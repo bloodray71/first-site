d28a24b9003442a089aa9ad1ed06797e46fa1e06
bloodray71 <109420133+bloodray71@users.noreply.github.com>
2022-07-16 15:30:24 +0300

    file upload

---
+++ js/index.js
@@ -0,0 +1,59 @@
+function MorInfoFerst() {
+    var ferstbtn=document.getElementById("ferst-btn");
+    var ferst=document.getElementById("ferst");
+    if (ferst.style.display === "none") {
+        ferst.style.display ="inline";
+    } else {
+        ferst.style.display ="none";
+    }
+}
+
+function MorInfoSecond() {
+    var secondtbtn=document.getElementById("second-btn");
+    var second=document.getElementById("second");
+    if (second.style.display === "none") {
+        second.style.display ="inline";
+    } else {
+        second.style.display ="none";
+    }
+}
+
+function MorInfoThird() {
+    var thirdbtn=document.getElementById("third-btn");
+    var third=document.getElementById("third");
+    if (third.style.display === "none") {
+        third.style.display ="inline";
+    } else {
+        third.style.display ="none";
+    }
+}
+
+function MorScooterInfoFerst() {
+    var ferstbtn=document.getElementById("scooter-btn-ferst");
+    var ferst=document.getElementById("ferst-scooter");
+    if (ferst.style.display === "none") {
+        ferst.style.display ="inline";
+    } else {
+        ferst.style.display ="none";
+    }
+}
+
+function MorScooterInfoSecond() {
+    var ferstbtn=document.getElementById("scooter-btn-second");
+    var ferst=document.getElementById("second-scooter");
+    if (ferst.style.display === "none") {
+        ferst.style.display ="inline";
+    } else {
+        ferst.style.display ="none";
+    }
+}
+
+function MorScooterInfoThird() {
+    var ferstbtn=document.getElementById("scooter-btn-third");
+    var ferst=document.getElementById("third-scooter");
+    if (ferst.style.display === "none") {
+        ferst.style.display ="inline";
+    } else {
+        ferst.style.display ="none";
+    }
+}
